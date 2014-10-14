6dfaae26a43c6f21ed6c9558005fd12f0379c5d4
markengelnmac <markengelnmac@sparemac01s-MacBook-Air.local>
2014-10-14 14:09:33 -0700

    swapped data.table for dplyr

--- R/BMICSCI.r
+++ R/BMICSCI.r
@@ -6,24 +6,24 @@
 #' 
 #' @param x An object of class BMIagg or BMIprc
 #' @export BMICSCI
-#' @import data.table
+#' @import dplyr
 #' @import vegan
 #' @import plyr
+
 
 BMICSCI <- function(x, effort=2){
   stopifnot("BMIagg" %in% class(x))
   x <- data.table(x[[effort]])
   x$Habit <- as.character(x$Habit)
-  x <- rename(x, c("distinct_SAFIT1" = "distinct_SAFIT2", "SAFIT2" = "iggSAFIT2", "SAFIT1" = "SAFIT2"))
-  result <- x[, list(
-    Taxonomic_Richness = nrow(.SD[distinct_SAFIT2=="Distinct"]),
-    Intolerant_Percent = sum(BAResult[which(ToleranceValue <= 2)])/sum(BAResult[!is.na(ToleranceValue)]),
-    Shredder_Taxa = nrow(.SD[distinct_SAFIT2=="Distinct" & (FunctionalFeedingGroup == "SH")]),
-    Clinger_PercentTaxa = nrow(.SD[distinct_SAFIT2=="Distinct" & (Habit == "CN")])/nrow(.SD[distinct_SAFIT2=="Distinct" & !is.na(Habit)]),
-    Coleoptera_PercentTaxa = nrow(.SD[distinct_SAFIT2=="Distinct" & (Order == "Coleoptera")])/nrow(.SD[distinct_SAFIT2=="Distinct"]), 
-    EPT_PercentTaxa = nrow(.SD[distinct_SAFIT2=="Distinct" & (Order %in% c("Ephemeroptera", "Plecoptera", "Trichoptera"))])/nrow(.SD[distinct_SAFIT2=="Distinct"])
-  ),
-  by=SampleID]
-  data.frame(result)
+  x <- plyr::rename(x, c("distinct_SAFIT1" = "distinct_SAFIT2", "SAFIT2" = "iggSAFIT2", "SAFIT1" = "SAFIT2"),
+              warn_missing=FALSE)
+  dplyr::summarise(group_by(x, SampleID), 
+    Taxonomic_Richness = sum1(distinct_SAFIT2=="Distinct"),
+    Intolerant_Percent = sum1(BAResult[which(ToleranceValue <= 2)])/sum1(BAResult[!is.na(ToleranceValue)]),
+    Shredder_Taxa = sum1(distinct_SAFIT2=="Distinct" & (FunctionalFeedingGroup == "SH")),
+    Clinger_PercentTaxa = sum1(distinct_SAFIT2=="Distinct" & Habit == "CN")/sum1(distinct_SAFIT2=="Distinct" & !is.na(Habit)),
+    Coleoptera_PercentTaxa = sum1(distinct_SAFIT2=="Distinct" & (Order == "Coleoptera"))/sum1(distinct_SAFIT2=="Distinct"), 
+    EPT_PercentTaxa = sum1(distinct_SAFIT2=="Distinct" & (Order %in% c("Ephemeroptera", "Plecoptera", "Trichoptera")))/sum1(distinct_SAFIT2=="Distinct")
+  )
 }
 
