4ddd8c7249f00182280a9c6299075a74c2c7b358
Robert Butler <robertb@sccwrp.org>
2024-11-19 16:06:29 -0800

    allow user to choose 2024 or 2025 metadata

--- R/loadMetaData.r
+++ R/loadMetaData.r
@@ -9,7 +9,10 @@ loadMetaData <- function(metadata.year = 2025){
   if (!(metadata.year %in% c(2024,2025))) {
     stop("Valid metadata years would be 2024 and 2025")
   }
-  load(system.file( paste0("metadata_STE", metadata.year, ".rdata"), package="BMIMetrics"))
+  
+  filename <- paste0("metadata_STE", metadata.year, ".rdata")
+  
+  load(system.file( filename, package="BMIMetrics"))
   #metadata <- taxonomy
   metadata
 }
