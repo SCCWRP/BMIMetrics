37ef1c26eab0040d144241478260dd8dd5645177
mkichinose-4 <mkichinose@csu.fullerton.edu>
2026-04-22 10:02:17 -0700

    Tested the updated metadata and nothing broke

--- R/update_metadata_042226.R
+++ R/update_metadata_042226.R
@@ -4,3 +4,9 @@ library(readxl)
 data <- read_xlsx("New CSCI metadata_032726.xlsx")
 
 save(data, file = 'inst/metadata_1.2.3.RData')
+
+install.packages('devtools')
+library(devtools)
+install_github('SCCWRP/BMIMetrics') # <--- This is a dependency that will not automatically install when CSCI is installed
+install_github('SCCWRP/CSCI')
+library(CSCI)
