552cd7a799d7540ede2b89f988981b88bff9ef15
Robert Butler <robertb@sccwrp.org>
2026-05-29 12:06:47 -0700

    update name of metadata variable for 2026

--- R/loadMetaData.r
+++ R/loadMetaData.r
@@ -5,9 +5,9 @@
 #' @export 
 #' 
 
-loadMetaData <- function(metadata.year = 2025){
-  if (!(metadata.year %in% c(2011,2025))) {
-    stop("Valid metadata years would be 2011 and 2025")
+loadMetaData <- function(metadata.year = 2026){
+  if (!(metadata.year %in% c(2011, 2025, 2026))) {
+    stop("Valid metadata years would be 2011, 2025, or 2026")
   }
   
   filename <- paste0("metadata_STE", metadata.year, ".rdata")
